9984047f8dcb71ef6144a35ba98584bbc40373cf
m_enomoto <m_enomoto@plus-one.tech>
2025-01-11 19:44:19 +0900

    GitHub Pagesにはデモを表示するように修正

--- vite.demo.config.ts
+++ vite.demo.config.ts
@@ -4,9 +4,10 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   root: 'src/demo',
+  base: '/talk-avatar/',
   plugins: [vue()],
   build: {
-    outDir: 'demo',
+    outDir: '../../docs',
     emptyOutDir: true
   },
 })
